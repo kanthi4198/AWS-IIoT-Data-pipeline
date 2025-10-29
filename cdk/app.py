d570ab09d25b17cb6c153cbeaa900ec679a5cec2
kanthi4198 <kanthimathinathan.a@gmail.com>
2025-10-29 20:12:25 +0100

    Adding .env and example .env files

--- cdk/app.py
+++ cdk/app.py
@@ -1,10 +1,18 @@
+import os
+from dotenv import load_dotenv
 import aws_cdk as cdk
 from smart_factory_iot_stack import SmartFactoryStack
+
+# Load environment variables from `.env` file
+load_dotenv()
+
+account = os.environ.get("CDK_DEFAULT_ACCOUNT")
+region = os.environ.get("CDK_DEFAULT_REGION")
 
 app = cdk.App()
 
 SmartFactoryStack(app, "SmartFactoryStack",
-    env=cdk.Environment(account="218508884047", region="eu-north-1")
+    env=cdk.Environment(account=account, region=region)
 )
 
 app.synth()
