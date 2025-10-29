f3be8ef62780472b739a60cea5afb2323fa1d514
kanthi4198 <kanthimathinathan.a@gmail.com>
2025-10-29 17:20:33 +0100

    AWS glue catalog added for IoT S3 files

--- cdk/smart_factory_iot_stack.py
+++ cdk/smart_factory_iot_stack.py
@@ -7,7 +7,9 @@ from aws_cdk import (
     aws_dynamodb as dynamodb,
     aws_events as events,
     aws_events_targets as targets,
+    aws_glue as glue,
     RemovalPolicy,
+
 )
 from constructs import Construct
 
@@ -90,8 +92,76 @@ class SmartFactoryStack(Stack):
         )
         hourly_rule.add_target(targets.LambdaFunction(lambda_batch))
 
+        #Glue catalog database and crawler
+        GLUE_DB_NAME = "smart_factory_db"
+        CRAWLER_NAME = "iot_hourly_crawler"
+        S3_PREFIX = "iot-data/"  # <-- ensure Lambda B writes CSVs here
 
+        # 1) Glue Data Catalog Database
+        glue_db = glue.CfnDatabase(
+            self, "SmartFactoryGlueDb",
+            catalog_id=self.account,
+            database_input=glue.CfnDatabase.DatabaseInputProperty(name=GLUE_DB_NAME),
+        )
 
+        # 2) IAM Role for Glue Crawler (minimal S3 read + managed Glue role)
+        glue_role = iam.Role(
+            self, "SmartFactoryGlueCrawlerRole",
+            assumed_by=iam.ServicePrincipal("glue.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AWSGlueServiceRole")
+            ],
+        )
+        glue_role.add_to_policy(iam.PolicyStatement(
+            actions=["s3:ListBucket", "s3:GetBucketLocation"],
+            resources=[bucket.bucket_arn],
+        ))
+        glue_role.add_to_policy(iam.PolicyStatement(
+            actions=["s3:GetObject"],
+            resources=[f"{bucket.bucket_arn}/*"],
+        ))
+
+        # 3) Glue Crawler scanning your batched CSVs in S3
+        s3_path = f"s3://{bucket.bucket_name}/{S3_PREFIX}"
+        crawler = glue.CfnCrawler(
+            self, "SmartFactoryGlueCrawler",
+            name=CRAWLER_NAME,
+            role=glue_role.role_arn,
+            database_name=GLUE_DB_NAME,
+            targets=glue.CfnCrawler.TargetsProperty(
+                s3_targets=[glue.CfnCrawler.S3TargetProperty(path=s3_path)]
+            ),
+            schema_change_policy=glue.CfnCrawler.SchemaChangePolicyProperty(
+                delete_behavior="LOG",
+                update_behavior="UPDATE_IN_DATABASE",
+            ),
+            recrawl_policy=glue.CfnCrawler.RecrawlPolicyProperty(
+                recrawl_behavior="CRAWL_EVERYTHING",
+            ),
+           
+        )
+        crawler.add_dependency(glue_db)
+
+        # 4) EventBridge rule to start Glue Crawler shortly after Lambda B runs
+
+        # Lambda B runs at minute 0; crawler starts 5 minutes later.
+        crawler_rule = events.Rule(
+            self, "StartGlueCrawlerHourly",
+            schedule=events.Schedule.cron(minute="5", hour="*"),
+        )
+        crawler_rule.add_target(
+            targets.AwsApi(
+                service="Glue",
+                action="startCrawler",
+                parameters={"Name": CRAWLER_NAME},
+                policy_statement=iam.PolicyStatement(
+                    actions=["glue:StartCrawler"],
+                    resources=["*"],
+                ),
+            )
+        )
+
+        
 
 
 
